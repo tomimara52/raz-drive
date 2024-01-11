b04dd573ba245686d6889be2bfa9221addbdca95
Tomás Maraschio <tomas.maraschio@gmail.com>
2024-01-11 17:22:37 -0300

    style delete file button
    thank you css grid

--- resources/js/Components/DeleteFileButton.jsx
+++ resources/js/Components/DeleteFileButton.jsx
@@ -1,17 +1,18 @@
 import PrimaryButton from '@/Components/PrimaryButton';
 import { router } from '@inertiajs/react';
 
-export default function DeleteFileButton({ className='', fileId,  ...props }) {
+export default function DeleteFileButton({ buttonClassName='', className='', fileName, fileId,  ...props }) {
     const submit = e => {
         e.preventDefault();
 
-        router.post(route('files.destroy', fileId));
+        if (window.confirm(`Are you sure you want to delete ${fileName}?`))
+            router.post(route('files.destroy', fileId));
     }
 
     return <div {...props} className={className}>
         <form onSubmit={submit}>
             <input type="number" hidden readOnly value={fileId}/>
-            <PrimaryButton type="submit" disabled={false}>Delete file</PrimaryButton>
+            <PrimaryButton className={buttonClassName} type="submit" disabled={false}>Delete file</PrimaryButton>
         </form>
     </div>;
 }
--- resources/js/Pages/Files/Show.jsx
+++ resources/js/Pages/Files/Show.jsx
@@ -19,7 +19,7 @@ export default function Show({ auth, file, content }) {
             }
 
             return (
-                <div className="flex justify-center m-6">
+                <div className="flex justify-center mx-6 p-4">
                     <div className="w-5/6 bg-slate-300 p-4 rounded">
                         {paragraphs}
                     </div>
@@ -36,17 +36,24 @@ export default function Show({ auth, file, content }) {
             header={<h2 className="font-semibold text-xl text-gray-800 leading-tight">{file.filepath}</h2>}
         >
             <Head title={file.filepath + ' '} />
-            <div className="p-4 flex items-baseline">
+            <div className="p-4 grid items-baseline" style={{gridTemplateColumns: '10% auto 10%'}}>
+                <div>
                     <Link
                         className="mr-1 bg-slate-300 border border-blue-500 rounded-sm p-2 text-cyan-700 hover:border-transparent hover:bg-gray-700 hover:text-white"
                         href={route('files.show', file.parent_dir)}
                     >
                         Back
                     </Link>
-                    <DeleteFileButton fileId={file.id} />
+                </div>
                 <p><b>File type:</b> {file.mimetype}</p>
+                <DeleteFileButton
+                    fileName={file.filepath.split('/').at(-1)}
+                    fileId={file.id}
+                    className="flex justify-end"
+                    buttonClassName="bg-red-300 border border-red-500 rounded-sm p-2 text-red-700 hover:border-transparent hover:bg-gray-700 hover:text-white"
+                />
             </div>
-                {getFileDisplayer()}
+            {getFileDisplayer()}
         </AuthenticatedLayout>
     );
 }
