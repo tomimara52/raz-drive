ccf737369720a9cb2412cf318c3a299ed5f3bdd2
Tomás Maraschio <tomas.maraschio@gmail.com>
2024-01-16 16:48:58 -0300

    add Log model with migration and controller

---
+++ app/Models/Log.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Log extends Model
+{
+    use HasFactory;
+
+    public function file(): BelongsTo
+    {
+        return $this->belongsTo(File::class);
+    }
+}
---
+++ database/migrations/2024_01_16_193239_create_logs_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use App\Models\File;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('logs', function (Blueprint $table) {
+            $table->id();
+            $table->foreignIdFor(File::class)->constrained()->cascadeOnUpdate();
+            $table->enum('type', ['create', 'update', 'delete']);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('logs');
+    }
+};
